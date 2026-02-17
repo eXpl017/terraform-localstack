4a645b859ede5b15ea823973ee62c6dbb6255a6b
eXpl017 <singhsnehal.2k1@gmail.com>
2026-02-17 11:53:04 +0000

    Changed to native s3 locks - removed dynamodb table created earlier - also made formatting changes

--- task0/main.tf
+++ task0/main.tf
@@ -22,20 +22,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_bucket_en
     }
   }
 }
-
-resource "aws_dynamodb_table" "tfstate_lock" {
-  name         = "tfstate-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
-}
-
-
