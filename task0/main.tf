7a4651e7fb8648e4cc87ace8f2948a6a46367c03
eXpl017 <singhsnehal.2k1@gmail.com>
2026-02-17 09:54:11 +0000

    Added remote backend for tfstate - created s3 bucket     - versioning enabled     - encrytion at rest enabled - created dynamodb for state-locking to maintain integrity

---
+++ task0/main.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "terraform-remote-state"
+
+  tags = {
+    Name = "Bucket to store terraform state remotely"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_bucket_ver" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_bucket_enc" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = "tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock Table"
+  }
+}
+
+
