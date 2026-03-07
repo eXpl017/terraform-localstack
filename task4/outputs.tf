95c1b56f1b86dcc9f6576ba4f544db2fc638156f
eXpl017 <singhsnehal.2k1@gmail.com>
2026-03-07 05:52:29 +0000

    added outputs required in later tasks

--- task4/outputs.tf
+++ task4/outputs.tf
@@ -1,3 +1,13 @@
-output "alb_dns_name" {
-  value = aws_lb.myalb.dns_name
+output "alb_info" {
+  value = {
+        dns_name = aws_lb.myalb.dns_name,
+        arn = aws_lb.myalb.arn
+    }
+}
+
+output "generic_info" {
+    value = {
+        tg_arn = aws_lb_target_group.app_serv_tg.id,
+        app_serv_sg_id = local.app_serv_sg_id
+    }
 }
